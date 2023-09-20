70322775e2e70e739e27cd6807398fe5a7547e5c
Edison De Jesus Abreu <edisondja@gmail.com>
2023-09-20 10:56:22 -0400

    New changes

--- Reddit_Download.php
+++ Reddit_Download.php
@@ -16,34 +16,29 @@ class Reddit_Download
            ini_set('user_agent', 'My-Application/2.5');
            include('simplehtmldom/simple_html_dom.php');
 
-           // URL de la página que deseas analizar
            $url = $post_url;
            $src = "";
 
-           // Crea una instancia de Simple HTML DOM Parser
            $html = file_get_html($url);
 
-           // Encuentra todas las etiquetas <source> en la página
            $sourceTags = $html->find('source');
 
-           // Recorre todas las etiquetas <source> y captura el atributo src
            foreach ($sourceTags as $sourceTag) {
                $src = $sourceTag->src;
            }
-           // Libera la memoria
            $html->clear();
              $this->video_link = $src;
            return $src;
     }
 
-    public function download(string $save_as, string $preset = 'fast', int $crf = 20): bool
+    public function download(string $save_as): bool
     {
-        $try_ffmpeg = trim(shell_exec('type -P ffmpeg'));
-        if (empty($try_ffmpeg)) {
-            throw new Exception("FFmpeg not found on your system");
-        }
-        $command = "ffmpeg -i '$this->video_link}' -c:v libx264 -preset {$preset} -crf {$crf} {$save_as}.mp4";
-        shell_exec($command);
+     
+        //       echo "$this->video_link";
+
+        $command = "ffmpeg -i '$this->video_link' {$save_as}.mp4";
+        system($command);
+      
         return true;
     }
 
---
+++ example.php
@@ -0,0 +1,25 @@
+<?php
+
+    require('Reddit_Download.php');
+
+    $url = "https://www.reddit.com/r/argentina/comments/u16yij/cocodrilo_con_reflujo/";
+
+
+    $Reddit_download = new Reddit_Download();
+ 
+    $Reddit_download->getVideoLink($url);
+
+    $save_as = "videos/cocodrilos.mp4";
+
+   if( $Reddit_download->download($save_as)){
+
+    echo $save_as;
+
+
+   }
+
+
+
+
+
+?>
